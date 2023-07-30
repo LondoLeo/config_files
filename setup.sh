df4b264a11eb9908997aca34a1dc177f7f41c53e
Londo <Londo@londo.lond>
2023-07-30 14:31:25 +0200

    Added basic i3 Configs

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.config/nvim
-mv config_files/init.lua ~/.config/nvim
-mv config_files/.tmux.conf ~
+cp config_files/init.lua ~/.config/nvim
+cp config_files/.tmux.conf ~
+cp config_files/i3 ~/.config/
+cp config_files/i3status ~/.config/
