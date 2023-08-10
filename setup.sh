f8fa06ca1aae70c3712136a43d8ea4eaf9ab66a9
Marcus F <marcus@wl-r6-212.rz.tu-ilmenau.de>
2023-08-10 11:39:57 +0200

    polybar added

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y gcc g++ python3 feh compton rofi curl git i3
+sudo apt install -y gcc g++ python3 feh picom rofi curl git i3 i3lock i3lock-fancy polybar zsh nodejs npm
 sudo snap install nvim --classic
